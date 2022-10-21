366bbfb01da8ae3c2907dcd884ec31894514ce83
Mix-Liten <mixliten@gmail.com>
2022-10-21 22:12:18 +0800

    chore: fix assets path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-qr-code-generator/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/react-qr_code_generator/' : './',
 })
